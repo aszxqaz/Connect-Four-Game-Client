c580ac9f8c59f3ede80a0ef3dd8e4dfa8d527399
Maxim B <b.maxim.dev@gmail.com>
2023-02-09 04:36:52 +0400

    template added

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { useState } from "react";
+import blackLayer from "./layer_black.svg";
+import whiteLayer from "./layer_white.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <>Hello</>
-  )
+    <div style={{ position: "relative" }}>
+      <img src={blackLayer} />
+      <div style={{ position: "absolute", top: 0 }}>
+        <img src={whiteLayer} />
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
